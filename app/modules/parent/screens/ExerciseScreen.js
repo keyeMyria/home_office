ebf318c9add02004056633998124376c17234c2d
Rayan Sóstenes <rayancocos@gmail.com>
2017-06-20 17:27:41 -0300

    Combo bom in Exercise Screen

--- app/modules/parent/screens/ExerciseScreen.js
+++ app/modules/parent/screens/ExerciseScreen.js
@@ -1,78 +1,139 @@
 import React, { Component } from 'react';
-import { TouchableWithoutFeedback, View } from 'react-native';
-import { Container, Header, Title, Content, Left, Right, Icon, Body, Text, Item, Label, Button } from 'native-base';
-
+import { TouchableWithoutFeedback, View, Dimensions } from 'react-native';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Left,
+    Right,
+    Icon,
+    Body,
+    Text,
+    Item,
+    Label,
+    Button,
+    Picker,
+} from 'native-base';
+import { Field, reduxForm } from 'redux-form';
 import { observer } from 'mobx-react/native';
+
 import store from '../../../store';
+import { PickerField } from '../../../components/fields';
 
 import { styles } from '../../../themes/educareTheme';
 
 import BubbleMenu from '../../../components/BubbleMenu';
 
+const ACTIVE_STYLE_MAP = {
+    true: {
+        ...styles.buttonActive,
+        width: (Dimensions.get('window').width / 3) - 15,
+    },
+    false: {
+        ...styles.buttonInactive,
+        width: (Dimensions.get('window').width / 3) - 15,
+    },
+};
+
 @observer
-export default class ExerciseScreen extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { subjectAreaIds: [] };
-  }
-
-  toogleSelectSubjectArea = (id) => {
-
-    let newSubjectAreaIds = this.state.subjectAreaIds;
-
-    if (this.subjectAreaIsSelected(id)) {
-      newSubjectAreaIds = newSubjectAreaIds.filter(subjectId => subjectId !== id);
-    } else {
-      newSubjectAreaIds.push(id);
+class ExerciseScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { subjectAreaIds: [] };
     }
 
-    this.setState({ subjectAreaIds: newSubjectAreaIds });
-  }
+    toogleSelectSubjectArea(id) {
+        const subjectAreaIds = this.state.subjectAreaIds.slice();
+        const index = subjectAreaIds.indexOf(id);
+        if (index === -1) {
+            subjectAreaIds.push(id);
+        } else {
+            subjectAreaIds.splice(index, 1);
+        }
 
-  subjectAreaIsSelected = id => this.state.subjectAreaIds.filter(subjectId => subjectId === id).length > 0;
+        this.setState({ subjectAreaIds });
+    }
 
-  render() {
+    subjectAreaIsSelected(id) {
+        return this.state.subjectAreaIds.indexOf(id) !== -1;
+    }
 
-    const { navigate } = this.props.navigation;
+    rendersubjectAreas() {
+        const mapFunc = (subject, index) => {
+            const onPress = () => this.toogleSelectSubjectArea(subject.id);
+            const active = this.subjectAreaIsSelected(subject.id);
+            const styleActive = ACTIVE_STYLE_MAP[active];
 
-    return (
-      <Container>
-        <Header appHeader>
-          <Left>
-            <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
-              <Icon name='menu' />
-            </TouchableWithoutFeedback>
-          </Left>
-          <Body>
-            <Title>Exercícios</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <BubbleMenu />
-          <Content padder>
+            return (
+              <Button key={index} rounded onPress={onPress} style={styleActive}>
+                <Text style={{ fontSize: 12 }}>{subject.name}</Text>
+              </Button>
+            );
+        };
+
+        return (
+          <View>
             <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
               <Label>Disciplinas a Executar Simulados</Label>
             </Item>
             <View style={styles.buttonView}>
-              {store.subjectAreas.map((subject, index) =>
-                <Button key={index} rounded
-                  onPress={() => this.toogleSelectSubjectArea(subject.id)}
-                  style={this.subjectAreaIsSelected(subject.id) ? styles.buttonActive : styles.buttonInactive}>
-                  <Text>{subject.name}</Text>
-                </Button>
-              )}
+              {store.subjectAreas.map(mapFunc)}
             </View>
-            <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
-              <Label>{'Você tem ' + this.state.subjectAreaIds.length + ' disciplina(s) selecionada(s)!'}</Label>
-            </Item>
-            <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
-              <Label>Horários para realização das listas</Label>
-            </Item>
-          </Content>
-        </Content>
-      </Container>
-    );
-  }
+          </View>
+        );
+    }
+
+    renderLoadExerciceField() {
+        const pickerItems = store.planningTimes.map((time, index) =>
+          <Picker.Item key={index} label={time.label} value={time.id} />,
+        );
+        const props = {
+            name: 'ExerciseLoad',
+            label: 'Carga Para o Simulado',
+            component: PickerField,
+            props: {
+                initialValue: 1,
+            },
+        };
+
+        return <View><Field {...props}>{pickerItems}</Field></View>;
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return (
+          <Container>
+            <Header appHeader>
+              <Left>
+                <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
+                  <Icon name="menu" />
+                </TouchableWithoutFeedback>
+              </Left>
+              <Body>
+                <Title>Exercícios</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content>
+              <BubbleMenu />
+              <Content padder>
+                {this.renderLoadExerciceField()}
+                {this.rendersubjectAreas()}
+                <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
+                  <Label>
+                    {`Você tem ${this.state.subjectAreaIds
+                                    .length} disciplina(s) selecionada(s)!`}
+                  </Label>
+                </Item>
+                <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
+                  <Label>Horários para realização das listas</Label>
+                </Item>
+              </Content>
+            </Content>
+          </Container>
+        );
+    }
 }
+export default reduxForm({ form: 'formExerciseScreen' })(ExerciseScreen);
