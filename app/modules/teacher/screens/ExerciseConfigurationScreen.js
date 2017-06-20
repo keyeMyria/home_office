39c179dbf7e519b5b4a4f077394f7c8161b90a5b
Rayan Sóstenes <rayancocos@gmail.com>
2017-06-20 17:27:59 -0300

    Exercise Configuration

--- app/modules/teacher/screens/ExerciseConfigurationScreen.js
+++ app/modules/teacher/screens/ExerciseConfigurationScreen.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
-import { TouchableWithoutFeedback, Modal, View } from 'react-native';
-import { Container, Header, Title, Content, Left, Right, Icon, Body, Picker, Text, Button, Item, Label } from 'native-base';
+import { TouchableWithoutFeedback, Modal, View, Dimensions } from 'react-native';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Left,
+    Right,
+    Icon,
+    Body,
+    Picker,
+    Text,
+    Button,
+    Item,
+    Label,
+} from 'native-base';
 import { Field, reduxForm } from 'redux-form';
 import _ from 'underscore';
 
@@ -13,120 +27,189 @@ import { PickerField } from '../../../components/fields';
 import SetDateForClassScreen from './SetDateForClassScreen';
 import SelectQuestionScreen from './SelectQuestionScreen';
 
+const QUESTION_QTDE_PICKER = _.range(0, 51, 1).map((val, index) => {
+    const label = val === 0 ? 'Nenhuma' : val + (val === 1 ? ' Questão' : ' Questões');
+    return <Picker.Item key={index} label={label} value={val + 1} />;
+});
+
+const ACTIVE_STYLE_MAP = {
+    true: {
+        ...styles.buttonActive,
+        width: Dimensions.get('window').width / 3 - 15,
+    },
+    false: {
+        ...styles.buttonInactive,
+        width: Dimensions.get('window').width / 3 - 15,
+    },
+};
+
 @observer
 class ExerciseConfigurationScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: props.visible,
+            setDateForClassScreenVisible: false,
+            selectQuestionScreenVisible: false,
+            questionGenerationTypeId: 1,
+            questionDatabaseTypeId: 1,
+        };
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: props.visible,
-      setDateForClassScreenVisible: false,
-      selectQuestionScreenVisible: false,
-      questionGenerationTypeId: 1,
-      questionDatabaseTypeId: 1,
-    };
-  }
+    componentWillReceiveProps(nextProps) {
+        this.setState({ visible: nextProps.visible });
+    }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ visible: nextProps.visible });
-  }
+    showSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: true });
+    }
 
-  showSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: true });
-  showSelectQuestionScreen = () => this.setState({ selectQuestionScreenVisible: true });
-  hideSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: false });
-  hideSelectQuestionScreen = () => this.setState({ selectQuestionScreenVisible: false });
+    showSelectQuestionScreen() {
+        this.setState({ selectQuestionScreenVisible: true });
+    }
 
-  render() {
+    hideSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: false });
+    }
 
-    const questionCountItems = _.range(0, 51, 1).map((val, index) => {
-      const label = val === 0 ? 'Nenhuma' : val + (val === 1 ? ' Questão' : ' Questões');
-      return (
-        <Picker.Item key={index} label={label} value={val + 1} />
-      );
-    });
+    hideSelectQuestionScreen() {
+        this.setState({ selectQuestionScreenVisible: false });
+    }
 
-    const questionGenerationTypeId = this.state.questionGenerationTypeId || store.questionGenerationTypes[0].id;
-    const showNextScreen = questionGenerationTypeId === 3 ? this.showSelectQuestionScreen : this.showSetDateForClassScreen;
+    /**
+     * Render the field to choose the Question DataBase, used to generate the
+     * Exercise
+     */
+    renderQuestionDatabaseTypes() {
+        const { questionDatabaseTypeId } = this.state;
 
-    return (
-      <Modal animationType={'slide'} transparent={false} visible={this.state.visible}>
-        <Container>
-          <Header appHeader>
-            <Left>
-              <TouchableWithoutFeedback onPress={this.props.hideModal}>
-                <Icon name='arrow-back' />
-              </TouchableWithoutFeedback>
-            </Left>
-            <Body>
-              <Title>Exercícios</Title>
-            </Body>
-            <Right>
-              <TouchableWithoutFeedback onPress={showNextScreen}>
-                <Text>Próximo</Text>
-              </TouchableWithoutFeedback>
-            </Right>
-          </Header>
-          <Content padder>
-            <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
-              <Label>Banco de Questões</Label>
-            </Item>
-            <View style={styles.buttonView}>
-              {store.questionDatabaseTypes.map((type, index) =>
-                <Button key={index} rounded
-                  onPress={() => this.setState({ questionDatabaseTypeId: type.id })}
-                  style={type.id === this.state.questionDatabaseTypeId ? styles.buttonActive : styles.buttonInactive}>
-                  <Text>{type.name}</Text>
-                </Button>
-              )}
-            </View>
-            <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
-              <Label>Modo de Geração</Label>
-            </Item>
-            <View style={styles.buttonView}>
-              {store.questionGenerationTypes.map((type, index) =>
-                <Button key={index} rounded
-                  onPress={() => this.setState({ questionGenerationTypeId: type.id })}
-                  style={type.id === this.state.questionGenerationTypeId ? styles.buttonActive : styles.buttonInactive}>
-                  <Text>{type.name}</Text>
-                </Button>
-              )}
-            </View>
-            {questionGenerationTypeId !== 3 &&
-              <View>
-                <Field
-                  name="numQuestoesFaceis"
-                  label="Fáceis"
-                  component={PickerField}
-                  props={{ initialValue: 1 }}>
-                  {questionCountItems}
-                </Field>
-                <Field
-                  name="numQuestoesMedias"
-                  label="Médios"
-                  component={PickerField}
-                  props={{ initialValue: 1 }}>
-                  {questionCountItems}
-                </Field>
-                <Field
-                  name="numQuestoesDificeis"
-                  label="Difíceis"
-                  component={PickerField}
-                  props={{ initialValue: 1 }}>
-                  {questionCountItems}
-                </Field>
-              </View>
+        const mapFunc = (type, index) => {
+            const activeStyle = ACTIVE_STYLE_MAP[type.id === questionDatabaseTypeId];
+            const onPress = () => this.setState({ questionDatabaseTypeId: type.id });
+            return (
+              <Button key={index} rounded onPress={onPress} style={activeStyle}>
+                <Text style={{ textAlign: 'center', fontSize: 12 }}>{type.name}</Text>
+              </Button>
+            );
+        };
+
+        return store.questionDatabaseTypes.map(mapFunc);
+    }
+
+    /**
+     * Render the field to choose the generation mode
+     */
+    renderQuestionGenerationTypes() {
+        const { questionGenerationTypeId } = this.state;
+        const mapFunc = (type, index) => {
+            const onPress = () => this.setState({ questionGenerationTypeId: type.id });
+            const activeStyle = ACTIVE_STYLE_MAP[type.id === questionGenerationTypeId];
+
+            return (
+              <Button key={index} rounded onPress={onPress} style={activeStyle}>
+                <Text style={{ textAlign: 'center', fontSize: 12 }}>{type.name}</Text>
+              </Button>
+            );
+        };
+
+        return store.questionGenerationTypes.map(mapFunc);
+    }
+
+    /**
+     * Renderiza o campo para escolher as dificuldades das questões
+     */
+    renderQuestionDificultPicker() {
+        const { questionGenerationTypeId } = this.state;
+        if (questionGenerationTypeId === 3) {
+            return false;
+        }
+        const props = { initialValue: 1 };
+
+        return (
+          <View>
+            <Field
+              name="numQuestoesFaceis"
+              label="Fáceis"
+              component={PickerField}
+              props={props}
+            >
+              {QUESTION_QTDE_PICKER}
+            </Field>
+            <Field
+              name="numQuestoesMedias"
+              label="Médios"
+              component={PickerField}
+              props={props}
+            >
+              {QUESTION_QTDE_PICKER}
+            </Field>
+            <Field
+              name="numQuestoesDificeis"
+              label="Difíceis"
+              component={PickerField}
+              props={props}
+            >
+              {QUESTION_QTDE_PICKER}
+            </Field>
+          </View>
+        );
+    }
+
+    render() {
+        const { questionGenerationTypeId, visible } = this.state;
+        const showNextScreen = () => {
+            if (questionGenerationTypeId === 3) {
+                this.showSelectQuestionScreen();
+            } else {
+                this.showSetDateForClassScreen();
             }
-          </Content>
-        </Container>
-        <SetDateForClassScreen
-          visible={this.state.setDateForClassScreenVisible}
-          hideModal={this.hideSetDateForClassScreen} />
-        <SelectQuestionScreen
-          visible={this.state.selectQuestionScreenVisible}
-          hideModal={this.hideSelectQuestionScreen} />
-      </Modal >
-    );
-  }
+        };
+
+        return (
+          <Modal animationType={'slide'} transparent={false} visible={visible}>
+            <Container>
+              <Header appHeader>
+                <Left>
+                  <TouchableWithoutFeedback onPress={this.props.hideModal}>
+                    <Icon name="arrow-back" />
+                  </TouchableWithoutFeedback>
+                </Left>
+                <Body>
+                  <Title>Exercícios</Title>
+                </Body>
+                <Right>
+                  <TouchableWithoutFeedback onPress={showNextScreen}>
+                    <Text>Próximo</Text>
+                  </TouchableWithoutFeedback>
+                </Right>
+              </Header>
+              <Content padder>
+                <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
+                  <Label>Banco de Questões</Label>
+                </Item>
+                <View style={styles.buttonView}>
+                  {this.renderQuestionDatabaseTypes()}
+                </View>
+                <Item stackedLabel style={{ borderBottomWidth: 0, marginBottom: 10 }}>
+                  <Label>Modo de Geração</Label>
+                </Item>
+                <View style={styles.buttonView}>
+                  {this.renderQuestionGenerationTypes()}
+                </View>
+                {this.renderQuestionDificultPicker()}
+              </Content>
+            </Container>
+            <SetDateForClassScreen
+              visible={this.state.setDateForClassScreenVisible}
+              hideModal={() => this.hideSetDateForClassScreen()}
+            />
+            <SelectQuestionScreen
+              visible={this.state.selectQuestionScreenVisible}
+              hideModal={() => this.hideSelectQuestionScreen()}
+            />
+          </Modal>
+        );
+    }
 }
 
 export default reduxForm({ form: 'formExerciseConfigurationScreen' })(ExerciseConfigurationScreen);
