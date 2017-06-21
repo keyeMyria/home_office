4b73e65b23c1304fb8f2c931660bd49635bd95cd
Rayan Sóstenes <rayancocos@gmail.com>
2017-06-21 17:43:52 -0300

    Add a combo box for the total grade and bimester for that test

--- app/modules/teacher/screens/ExamScreen.js
+++ app/modules/teacher/screens/ExamScreen.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
-import { Container, Header, Title, Content, Left, Right, Icon, Body, Picker, Text, Item, ListItem, CheckBox, Label } from 'native-base';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Left,
+    Right,
+    Icon,
+    Body,
+    Picker,
+    Text,
+    Item,
+    ListItem,
+    CheckBox,
+    Label,
+} from 'native-base';
 import { connect } from 'react-redux';
 import { Field, reduxForm, formValueSelector } from 'redux-form';
 
@@ -14,106 +29,145 @@ import SetDateForClassScreen from './SetDateForClassScreen';
 
 @observer
 class ExamScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { setDateForClassScreenVisible: false };
+        this.showSetDateForClassScreen = this.showSetDateForClassScreen.bind(this);
+        this.hideSetDateForClassScreen = this.hideSetDateForClassScreen.bind(this);
+        this.checkUncheckTopic = this.checkUncheckTopic.bind(this);
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = { setDateForClassScreenVisible: false };
-  }
-  
-  showSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: true });
-  hideSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: false });
+    showSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: true });
+    }
 
-  checkUncheckTopic = (checked, topicId) => {
-    checked ?
-      store.uncheckExamTopic(topicId) :
-      store.checkExamTopic(topicId);
-  }
+    hideSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: false });
+    }
 
-  render() {
+    checkUncheckTopic(checked, topicId) {
+        if (checked) {
+            store.uncheckExamTopic(topicId);
+        } else {
+            store.checkExamTopic(topicId);
+        }
+    }
 
-    const { navigate } = this.props.navigation;
+    getGradesItems() {
+        const mapFunc = (grade, index) => <Picker.Item key={index} label={`${grade} Pontos`} value={grade} />;
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(mapFunc);
+    }
 
-    const subjectAreaItems = store.teacher.subjectAreas.map((subject, index) =>
-      <Picker.Item key={index} label={subject.name} value={subject.id} />
-    );
+    getSubjectAreaItems() {
+        const mapFunc = (subject, index) =>
+          <Picker.Item key={index} label={subject.name} value={subject.id} />;
+        return store.teacher.subjectAreas.map(mapFunc);
+    }
 
-    const periodItems = store.periods.map((period, index) =>
-      <Picker.Item key={index} label={period.name} value={period.id} />
-    );
+    getPeriodItems() {
+        const mapFunc = (period, index) =>
+          <Picker.Item key={index} label={period.name} value={period.id} />;
+        return store.periods.map(mapFunc);
+    }
 
-    let subjectAreas = store.teacher.subjectAreas.filter(subject => subject.id === this.props.subjectAreaId);
-    const topics = subjectAreas.length > 0 ? subjectAreas[0].topics : store.teacher.subjectAreas[0].topics;
+    getSubjectAreas() {
+        const { subjectAreaId } = this.props;
+        return store.teacher.subjectAreas.filter(subject => subject.id === subjectAreaId);
+    }
 
-    return (
-      <Container>
-        <Header appHeader>
-          <Left>
-            <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
-              <Icon name='menu' />
-            </TouchableWithoutFeedback>
-          </Left>
-          <Body>
-            <Title>Provas</Title>
-          </Body>
-          <Right>
-            <TouchableWithoutFeedback onPress={this.showSetDateForClassScreen}>
-              <Text>Próximo</Text>
-            </TouchableWithoutFeedback>
-          </Right>
-        </Header>
-        <Content>
-          <BubbleMenu mode="schoolYear" />
-          <Content padder>
-            <Field
-              name="disciplina"
-              label="Disciplina"
-              component={PickerField}
-              props={{ initialValue: 1 }}>
-              {subjectAreaItems}
-            </Field>
-            <Field
-              name="periodo"
-              label="Bimestre"
-              component={PickerField}
-              props={{ initialValue: 1 }}>
-              {periodItems}
-            </Field>
-            <Item stackedLabel style={{ borderBottomWidth: 0 }}>
-              <Label>Selecione os Tópicos</Label>
-            </Item>
-          </Content>
-          {topics.map((topic, index) => {
-            const checked = store.examTopics.filter(topicId => topicId === topic.id).length > 0;
-            return (
-              <ListItem key={index}
-                onPress={() => this.checkUncheckTopic(checked, topic.id)}>
-                <Body>
-                  <Text>{topic.name}</Text>
-                </Body>
-                <Right>
-                  <CheckBox
-                    checked={checked}
-                    style={{ marginRight: 20 }}
-                  />
-                </Right>
-              </ListItem>
-            );
-          })}
-        </Content>
-        <SetDateForClassScreen
-          visible={this.state.setDateForClassScreenVisible}
-          hideModal={this.hideSetDateForClassScreen} />
-      </Container>
-    );
-  }
+    getTopics() {
+        const subjectAreas = this.getSubjectAreas();
+        if (subjectAreas.length) {
+            return subjectAreas[0].topics;
+        }
+
+        return store.teacher.subjectAreas[0].topics;
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        const subjectAreaItems = this.getSubjectAreaItems();
+        const periodItems = this.getPeriodItems();
+        const topics = this.getTopics();
+        const gradesItems = this.getGradesItems();
+
+        return (
+          <Container>
+            <Header appHeader>
+              <Left>
+                <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
+                  <Icon name="menu" />
+                </TouchableWithoutFeedback>
+              </Left>
+              <Body>
+                <Title>Provas</Title>
+              </Body>
+              <Right>
+                <TouchableWithoutFeedback onPress={this.showSetDateForClassScreen}>
+                  <Text>Próximo</Text>
+                </TouchableWithoutFeedback>
+              </Right>
+            </Header>
+            <Content>
+              <BubbleMenu mode="schoolYear" />
+              <Content padder>
+                <Field
+                  name="disciplina"
+                  label="Disciplina"
+                  component={PickerField}
+                  props={{ initialValue: 1 }}
+                >
+                  {subjectAreaItems}
+                </Field>
+                <Field
+                  name="periodo"
+                  label="Bimestre"
+                  component={PickerField}
+                  props={{ initialValue: 1 }}
+                >
+                  {periodItems}
+                </Field>
+                <Field
+                  name="grade"
+                  label="Nota"
+                  component={PickerField}
+                  props={{ initialValue: 10 }}
+                >
+                  {gradesItems}
+                </Field>
+                <Item stackedLabel style={{ borderBottomWidth: 0 }}>
+                  <Label>Selecione os Tópicos</Label>
+                </Item>
+              </Content>
+              {topics.map((topic, index) => {
+                  const checked =
+                            store.examTopics.filter(topicId => topicId === topic.id).length > 0;
+                  return (
+                    <ListItem
+                      key={index}
+                      onPress={() => this.checkUncheckTopic(checked, topic.id)}
+                    >
+                      <Body>
+                        <Text>{topic.name}</Text>
+                      </Body>
+                      <Right>
+                        <CheckBox checked={checked} style={{ marginRight: 20 }} />
+                      </Right>
+                    </ListItem>
+                  );
+              })}
+            </Content>
+            <SetDateForClassScreen
+              visible={this.state.setDateForClassScreenVisible}
+              hideModal={this.hideSetDateForClassScreen}
+            />
+          </Container>
+        );
+    }
 }
 
 const form = reduxForm({ form: 'formExamScreen' })(ExamScreen);
 const selector = formValueSelector('formExamScreen');
-export default connect(
-  state => {
-    return {
-      subjectAreaId: selector(state, 'disciplina')
-    };
-  }
-)(form);
+export default connect(state => ({
+    subjectAreaId: selector(state, 'disciplina'),
+}))(form);
--- app/modules/teacher/screens/HomeworkScreen.js
+++ app/modules/teacher/screens/HomeworkScreen.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
-import { Container, Header, Title, Content, Left, Right, Icon, Body, Picker, Text } from 'native-base';
+import {
+    Container,
+    Header,
+    Title,
+    Content,
+    Left,
+    Right,
+    Icon,
+    Body,
+    Picker,
+    Text,
+} from 'native-base';
 import { Field, reduxForm } from 'redux-form';
 
 import { observer } from 'mobx-react/native';
@@ -13,65 +24,106 @@ import SetDateForClassScreen from './SetDateForClassScreen';
 
 @observer
 class HomeworkScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { setDateForClassScreenVisible: false };
+        this.showSetDateForClassScreen = this.showSetDateForClassScreen.bind(this);
+        this.hideSetDateForClassScreen = this.hideSetDateForClassScreen.bind(this);
+    }
 
-  constructor(props) {
-    super(props);
-    this.state = { setDateForClassScreenVisible: false };
-  }
+    showSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: true });
+    }
 
-  showSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: true });
-  hideSetDateForClassScreen = () => this.setState({ setDateForClassScreenVisible: false });
+    hideSetDateForClassScreen() {
+        this.setState({ setDateForClassScreenVisible: false });
+    }
 
-  render() {
+    getSubjectAreaItems() {
+        const mapFunc = (subject, index) =>
+          <Picker.Item key={index} label={subject.name} value={subject.id} />;
+        return store.teacher.subjectAreas.map(mapFunc);
+    }
 
-    const { navigate } = this.props.navigation;
+    getGradesItems() {
+        const mapFunc = (grade, index) =>
+          <Picker.Item key={index} label={`${grade} Pontos`} value={grade} />;
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(mapFunc);
+    }
 
-    const subjectAreaItems = store.teacher.subjectAreas.map((subject, index) =>
-      <Picker.Item key={index} label={subject.name} value={subject.id} />
-    );
+    getPeriodItems() {
+        const mapFunc = (period, index) =>
+          <Picker.Item key={index} label={period.name} value={period.id} />;
+        return store.periods.map(mapFunc);
+    }
 
-    return (
-      <Container>
-        <Header appHeader>
-          <Left>
-            <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
-              <Icon name='menu' />
-            </TouchableWithoutFeedback>
-          </Left>
-          <Body>
-            <Title>Trabalhos</Title>
-          </Body>
-          <Right>
-            <TouchableWithoutFeedback onPress={this.showSetDateForClassScreen}>
-              <Text>Próximo</Text>
-            </TouchableWithoutFeedback>
-          </Right>
-        </Header>
-        <Content>
-          <BubbleMenu mode="schoolYear" />
-          <Content padder>
-            <Field
-              name="disciplina"
-              label="Disciplina"
-              component={PickerField}
-              props={{ initialValue: 1 }}>
-              {subjectAreaItems}
-            </Field>
-            <Field
-              style={{ height: 150 }}
-              name="information"
-              label="Descrição da Atividade"
-              component={TextField}
-              multiline={true}>
-            </Field>
-          </Content>
-        </Content>
-        <SetDateForClassScreen
-          visible={this.state.setDateForClassScreenVisible}
-          hideModal={this.hideSetDateForClassScreen} />
-      </Container>
-    );
-  }
+    render() {
+        const { navigate } = this.props.navigation;
+
+        const subjectAreaItems = this.getSubjectAreaItems();
+        const gradesItems = this.getGradesItems();
+        const periodItems = this.getPeriodItems();
+
+        return (
+          <Container>
+            <Header appHeader>
+              <Left>
+                <TouchableWithoutFeedback onPress={() => navigate('DrawerOpen')}>
+                  <Icon name="menu" />
+                </TouchableWithoutFeedback>
+              </Left>
+              <Body>
+                <Title>Trabalhos</Title>
+              </Body>
+              <Right>
+                <TouchableWithoutFeedback onPress={this.showSetDateForClassScreen}>
+                  <Text>Próximo</Text>
+                </TouchableWithoutFeedback>
+              </Right>
+            </Header>
+            <Content>
+              <BubbleMenu mode="schoolYear" />
+              <Content padder>
+                <Field
+                  name="disciplina"
+                  label="Disciplina"
+                  component={PickerField}
+                  props={{ initialValue: 1 }}
+                >
+                  {subjectAreaItems}
+                </Field>
+                <Field
+                  name="periodo"
+                  label="Bimestre"
+                  component={PickerField}
+                  props={{ initialValue: 1 }}
+                >
+                  {periodItems}
+                </Field>
+                <Field
+                  name="grade"
+                  label="Nota"
+                  component={PickerField}
+                  props={{ initialValue: 10 }}
+                >
+                  {gradesItems}
+                </Field>
+                <Field
+                  style={{ height: 150 }}
+                  name="information"
+                  label="Descrição da Atividade"
+                  component={TextField}
+                  multiline
+                />
+              </Content>
+            </Content>
+            <SetDateForClassScreen
+              visible={this.state.setDateForClassScreenVisible}
+              hideModal={this.hideSetDateForClassScreen}
+            />
+          </Container>
+        );
+    }
 }
 
 export default reduxForm({ form: 'formHomeworkScreen' })(HomeworkScreen);
