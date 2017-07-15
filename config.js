480ff6c194427c02e06359dedb751b84b60f7acd
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-14 22:25:20 -0300

    Externalize configurations

---
+++ config.js
@@ -0,0 +1,35 @@
+export default {
+    LOGIN: {
+        // BG_IMG: require('./app/img/bg.jpg'), // eslint-disable-line global-require
+        // ICON_IMG: require('./app/img/logo.png'), // eslint-disable-line global-require
+    },
+    API: {
+        DOMAIN: 'http://escola.educarebox.com/',
+        get BASE_URL() {
+            return `${this.DOMAIN}api/`;
+        },
+        get AUTH_URL() {
+            return `${this.DOMAIN}auth/`;
+        },
+    },
+    AGENDA: {
+        tipoColorMap: {
+            TRABALHO: '#808080',
+            PROVA: '#E57373',
+            EXERCICIO: '#64B5F6',
+            LISTA_ONLINE: '#FFF176',
+        },
+        tipoAbbrMap: {
+            TRABALHO: 'T',
+            PROVA: 'P',
+            EXERCICIO: 'E',
+            LISTA_ONLINE: 'L',
+        },
+        tipoNameMap: {
+            TRABALHO: 'Trabalho',
+            PROVA: 'Prova',
+            EXERCICIO: 'Exercício',
+            LISTA_ONLINE: 'Lista Online',
+        },
+    },
+};
