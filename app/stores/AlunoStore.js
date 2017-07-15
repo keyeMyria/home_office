e76ee2cc01341b6d451ba05d2f4be0994a2d585f
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-14 22:21:56 -0300

    Store for aluno

---
+++ app/stores/AlunoStore.js
@@ -0,0 +1,28 @@
+import { observable, action, computed } from 'mobx';
+import { AlunoService } from './../services';
+import { Aluno } from './../models';
+import type { Nota } from './../models';
+
+class AlunoStore {
+    _service = new AlunoService();
+    @observable id: ?number;
+    @observable loading = false;
+    @observable aluno: Aluno;
+
+    @action
+    async fetchAluno(id) {
+        this.id = id;
+        this.loading = true;
+        const aluno = await this._service.one(this.id).get();
+        this.aluno = new Aluno(aluno);
+        this.loading = false;
+        return this;
+    }
+
+    @computed
+    get notas(): Array<Nota> {
+        return this.aluno ? this.aluno.notas : [];
+    }
+}
+
+export default new AlunoStore();
