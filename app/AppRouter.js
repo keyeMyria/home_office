870f9d9cc7ed053efb8f713a62058409457e30d0
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-21 19:05:39 -0300

    use moment locale pt-br

--- app/AppRouter.js
+++ app/AppRouter.js
@@ -1,20 +1,25 @@
 // @flow
 
 import { StackNavigator } from 'react-navigation';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 import LoginScreen from './screens/LoginScreen';
 import ParentHomeRouter from './modules/parent/AppRouter';
 import TeacherHomeRouter from './modules/teacher/AppRouter';
 import StudentHomeRouter from './modules/student/AppRouter';
 
+// Configure moment locale
+moment.locale('pt-br');
+
 export default StackNavigator(
-  {
-    LoginScreen: { screen: LoginScreen },
-    TeacherHomeRouter: { screen: TeacherHomeRouter },
-    ParentHomeRouter: { screen: ParentHomeRouter },
-    StudentHomeRouter: { screen: StudentHomeRouter },
-  },
-  {
-    headerMode: 'none',
-  }
+    {
+        LoginScreen: { screen: LoginScreen },
+        TeacherHomeRouter: { screen: TeacherHomeRouter },
+        ParentHomeRouter: { screen: ParentHomeRouter },
+        StudentHomeRouter: { screen: StudentHomeRouter },
+    },
+    {
+        headerMode: 'none',
+    },
 );
