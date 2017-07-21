3fa68fef68e80c2bdff82d593a75e489997b3a43
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-21 17:29:05 -0300

    Calendar week

--- app/components/calendar/CalendarWeek.js
+++ app/components/calendar/CalendarWeek.js
@@ -7,36 +7,35 @@ import { PropTypes } from 'mobx-react';
 import CalendarItem from './CalendarItem';
 
 export default class CalendarWeek extends Component {
-    static propTypes = {
-        items: PropTypes.arrayOrObservableArrayOf(CalendarItem.propTypes.item),
-        label: React.PropTypes.string.isRequired,
-        onPress: React.PropTypes.func.isRequired,
-    };
+  static defaultProps = {
+    onPress: () => {},
+  };
 
-    static defaultProps = {
-        onPress: () => {},
-    };
+  renderCalendarItems() {
+    const { items, onPress } = this.props;
 
-    renderCalendarItems() {
-        const { items, onPress } = this.props;
-        return items.map((item, index) =>
-          <CalendarItem key={index} item={item} onPress={onPress} />,
-        );
-    }
+    return (
+      items &&
+      items.map((item, index) =>
+        <CalendarItem key={index} item={item} onPress={onPress} />
+      )
+    );
+  }
 
-    render() {
-        const { items, label } = this.props;
+  render() {
+    const { items, label } = this.props;
 
-        return (
-          <View>
-            {items.length &&
-            <Separator>
-              <Text>
-                {label}
-              </Text>
-            </Separator>}
-            {this.renderCalendarItems()}
-          </View>
-        );
-    }
+    return (
+      <View>
+        {!!items &&
+          !!items.length &&
+          <Separator>
+            <Text>
+              {label}
+            </Text>
+          </Separator>}
+        {this.renderCalendarItems()}
+      </View>
+    );
+  }
 }
