a5dddb891129938353371da4ca84dfc275a91088
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-23 17:02:36 -0300

    Refactor teacher routes

--- app/modules/teacher/AppRouter.js
+++ app/modules/teacher/AppRouter.js
@@ -5,18 +5,14 @@ import { DrawerNavigator, TabNavigator } from 'react-navigation';
 import AppNavigator from './AppNavigator';
 import HomeNavigator from './HomeNavigator';
 import {
-    AlertScreen,
     ExerciseScreen,
-    MessageScreen,
-    HistoryScreen,
     SelectClassScreen,
     ExamScreen,
     HomeworkScreen,
-    AnalysisScreen,
-    CalendarScreen,
-    FeedBackScreen,
-    HelpScreen,
+    ComunicadosScreen,
 } from './screens';
+
+import { AlertScreen, CalendarScreen, FeedBackScreen, HelpScreen } from './../common_screens';
 
 const HomeRouter = TabNavigator(
     {
@@ -35,9 +31,7 @@ const HomeRouter = TabNavigator(
 export default DrawerNavigator(
     {
         HomeRouter: { screen: HomeRouter },
-        MessageScreen: { screen: MessageScreen },
-        HistoryScreen: { screen: HistoryScreen },
-        AnalysisScreen: { screen: AnalysisScreen },
+        ComunicadosScreen: { screen: ComunicadosScreen },
         SelectClassScreen: { screen: SelectClassScreen },
         FeedBackScreen: { screen: FeedBackScreen },
         HelpScreen: { screen: HelpScreen },
--- app/modules/teacher/HomeNavigator.js
+++ app/modules/teacher/HomeNavigator.js
@@ -1,60 +1,38 @@
+// @flow
 import React, { Component } from 'react';
-import { Footer, FooterTab, Button, Badge, Icon, Text } from 'native-base';
+import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
 import { observer } from 'mobx-react/native';
-import store from '../../store';
 
 @observer
 export default class HomeTabNavigator extends Component {
-
-  render() {
-
-    const { navigate, state: { index } } = this.props.navigation;
-    const alertBadgeCount = store.teacher.alerts != null ? store.teacher.alerts.items.filter(o => !o.readed).length : 0;
-
-    return (
-      <Footer>
-        <FooterTab footerTabNavigation>
-          <Button
-            active={index === 0}
-            onPress={() => navigate('CalendarScreen')}>
-            <Icon name="event-note" />
-            <Text>Agenda</Text>
-          </Button>
-          <Button
-            active={index === 1}
-            onPress={() => navigate('ExerciseScreen')}>
-            <Icon name="tune" />
-            <Text>Exercícios</Text>
-          </Button>
-          <Button
-            active={index === 2}
-            onPress={() => navigate('AlertScreen')}
-            badge={alertBadgeCount > 0}>
-            {alertBadgeCount > 0 && <Badge><Text>{alertBadgeCount}</Text></Badge>}
-            <Icon name="notifications" />
-            <Text>Alertas</Text>
-          </Button>
-          <Button
-            active={index === 3}
-            onPress={() => navigate('ExamScreen')}>
-            <Icon name="assignment-turned-in" />
-            <Text>Provas</Text>
-          </Button>
-          <Button
-            active={index === 4}
-            onPress={() => navigate('HomeworkScreen')}>
-            <Icon name="library-books" />
-            <Text>Trabalhos</Text>
-          </Button>
-          {/*<Button
-            active={index === 4}
-            onPress={() => navigate('AnalysisScreen')}>
-            <Icon name="insert-chart" />
-            <Text>Análises</Text>
-          </Button>*/}
-        </FooterTab>
-      </Footer>
-    );
-  }
+    render() {
+        const { navigate, state: { index } } = this.props.navigation;
+        return (
+          <Footer>
+            <FooterTab footerTabNavigation>
+              <Button active={index === 0} onPress={() => navigate('CalendarScreen')}>
+                <Icon name="event-note" />
+                <Text>Agenda</Text>
+              </Button>
+              <Button active={index === 1} onPress={() => navigate('ExerciseScreen')}>
+                <Icon name="tune" />
+                <Text>Exercícios</Text>
+              </Button>
+              <Button active={index === 2} onPress={() => navigate('AlertScreen')}>
+                <Icon name="notifications" />
+                <Text>Avisos</Text>
+              </Button>
+              <Button active={index === 3} onPress={() => navigate('ExamScreen')}>
+                <Icon name="assignment-turned-in" />
+                <Text>Provas</Text>
+              </Button>
+              <Button active={index === 4} onPress={() => navigate('HomeworkScreen')}>
+                <Icon name="library-books" />
+                <Text>Trabalhos</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        );
+    }
 }
