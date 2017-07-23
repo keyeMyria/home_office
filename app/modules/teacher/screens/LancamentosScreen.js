3ad536c4e2cbad0f0a566ca185e17cc31eec5351
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-23 17:03:24 -0300

    refactoring teacher screens

--- app/modules/teacher/screens/LancamentosScreen.js
+++ app/modules/teacher/screens/LancamentosScreen.js
@@ -2,16 +2,24 @@
 import React, { Component } from 'react';
 import { Text } from 'native-base';
 
-import ScreenShell from '../../../components/ScreenShell';
-import BubbleMenu from '../../../components/BubbleMenu';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react/native';
 
+import ScreenShell from '../../../components/ScreenShell';
+
+@observer
 export default class MessageScreen extends Component {
+    @observable
+    store = {
+        visible: false,
+        title: '',
+    };
+
     render() {
         const { navigate } = this.props.navigation;
         return (
           <ScreenShell title="Mensagens" navigate={navigate} padder={false}>
-            <BubbleMenu />
-            <Text>Mensagens</Text>
+            <Text>Lancamentos</Text>
           </ScreenShell>
         );
     }
