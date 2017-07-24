7cc0410cd93bcd43ea4fe653ab707172e1dc1b33
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-24 04:47:25 -0300

    Returning ProvaStore and ExercicioStore

---
+++ app/stores/professor/ExercicioStore.js
@@ -0,0 +1,37 @@
+// @flow
+
+import { observable } from 'mobx';
+import { ExercicioService } from '../../services';
+import CONFIG from './../../../config';
+
+class ExercicioStore {
+    service: ExercicioService;
+    @observable exercicios: Array<any>; // TODO: Colocar o model especifico
+
+    constructor() {
+        this.service = new ExercicioService();
+    }
+
+    async load() {
+        const result = await this.service.get();
+        this.exercicios = result.exercicios;
+    }
+
+    async save(data: Object) {
+        const readyData = {
+            ...data,
+            disciplina: `${CONFIG.API.BASE_URL}/disciplinas/${data.disciplina}`,
+            titulo: 'Teste',
+            topicos: data.topicos
+                ? data.topicos.map(item => `${CONFIG.API.BASE_URL}/topicos/${item}`)
+                : [],
+        };
+        const result = await this.service.post(readyData);
+        if (__DEV__) console.log(result); // eslint-disable-line
+    }
+}
+
+const store = new ExercicioStore();
+store.load();
+
+export default store;
---
+++ app/stores/professor/ProvaStore.js
@@ -0,0 +1,37 @@
+// @flow
+
+import { observable } from 'mobx';
+import { ProvaService } from '../../services';
+import CONFIG from './../../../config';
+
+class ProvaStore {
+    service: ProvaService;
+    @observable provas: Array<any>; // TODO: Colocar o model especifico
+
+    constructor() {
+        this.service = new ProvaService();
+    }
+
+    async load() {
+        const result = await this.service.get();
+        this.provas = result.provas;
+    }
+
+    async save(data: Object) {
+        const readyData = {
+            ...data,
+            disciplina: `${CONFIG.API.BASE_URL}/disciplinas/${data.disciplina}`,
+            titulo: 'Teste',
+            topicos: data.topicos
+                ? data.topicos.map(item => `${CONFIG.API.BASE_URL}/topicos/${item}`)
+                : [],
+        };
+        const result = await this.service.post(readyData);
+        if (__DEV__) console.log(result); // eslint-disable-line
+    }
+}
+
+const store = new ProvaStore();
+store.load();
+
+export default store;
