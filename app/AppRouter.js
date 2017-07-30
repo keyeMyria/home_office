b9f56dfdca4c43cd4bf1711aa6057b0aeba9da73
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-30 14:00:00 -0300

    new screens in app router

--- app/AppRouter.js
+++ app/AppRouter.js
@@ -4,8 +4,14 @@ import { StackNavigator } from 'react-navigation';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
+// Load stores
+import './stores/UiStore';
+import './stores/UserStore';
+
 import LoginScreen from './screens/LoginScreen';
 import InitialScreen from './screens/InitialScreen';
+import CreateUserScreen from './screens/CreateUserScreen';
+import SplashScreen from './screens/SplashScreen';
 import ParentHomeRouter from './modules/parent/AppRouter';
 import TeacherHomeRouter from './modules/teacher/AppRouter';
 import StudentHomeRouter from './modules/student/AppRouter';
@@ -15,8 +21,10 @@ moment.locale('pt-br');
 
 export default StackNavigator(
     {
+        SplashScreen: { screen: SplashScreen },
         InitialScreen: { screen: InitialScreen },
         LoginScreen: { screen: LoginScreen },
+        CreateUserScreen: { screen: CreateUserScreen },
         TeacherHomeRouter: { screen: TeacherHomeRouter },
         ParentHomeRouter: { screen: ParentHomeRouter },
         StudentHomeRouter: { screen: StudentHomeRouter },
