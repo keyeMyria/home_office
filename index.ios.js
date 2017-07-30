95cf9622dacc7c5dfb54c81ffa80e61486f3e61b
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-30 14:00:00 -0300

    codepush no ios

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { StyleProvider } from 'native-base';
+import codePush from 'react-native-code-push';
 
 // Redux Form Store
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import pushHandler from './app/lib/push';
 import allReducers from './app/reducers';
-// Theme
-import { educareTheme } from './app/themes/educareTheme';
 
+import { educareTheme } from './app/themes/educareTheme';
 import AppRouter from './app/AppRouter';
 
 const formStore = createStore(allReducers);
 
+pushHandler.configureNotifications();
+
+@codePush
 class EducareApp extends Component {
     render() {
         return (
