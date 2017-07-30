34e118691ff2fbb2a857ba51e8280ceda44d13c6
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-30 14:00:00 -0300

    logger

---
+++ app/lib/logger.js
@@ -0,0 +1,28 @@
+// @flow
+
+function log(...args: Array<any>): void {
+    if (__DEV__) {
+        // eslint-disable-next-line no-console
+        console.log(...args);
+    }
+}
+
+function warn(...args: Array<any>): void {
+    if (__DEV__) {
+        // eslint-disable-next-line no-console
+        console.warn(...args);
+    }
+}
+
+function error(...args: Array<any>): void {
+    if (__DEV__) {
+        // eslint-disable-next-line no-console
+        console.warn(...args);
+    }
+}
+
+export default {
+    log,
+    error,
+    warn,
+};
