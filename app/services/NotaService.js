e0de6e3f5bc6a46055d8f92b8017a5cc75d9be7b
Rayan Sóstenes <rayancocos@gmail.com>
2017-07-30 14:00:00 -0300

    Nota Service

--- app/services/NotaService.js
+++ app/services/NotaService.js
@@ -8,23 +8,22 @@ export default class NotaService extends CollectionService {
     }
 
     findByEvento(id: number) {
-        return super.search({ id });
+        return super.search({ id }, 'findByEvento');
     }
 
     findByAluno(id: number) {
-        return super.search({ id });
+        return super.search({ id }, 'findByAluno');
     }
 
     findByAlunoAndTarefa(aluno: number, tarefa: number) {
-        return super.search({ aluno, tarefa });
+        return super.search({ aluno, tarefa }, 'findByAlunoAndTarefa');
     }
 
     findByTurmaAndTarefa(turma: number, tarefa: number) {
-        return super.search({ turma, tarefa });
+        return super.search({ turma, tarefa }, 'findByTurmaAndTarefa');
     }
 
     findByEventoAndAluno(evento: number, aluno: number) {
-        return super.search({ evento, aluno });
+        return super.search({ evento, aluno }, 'findByEventoAndAluno');
     }
-
 }
