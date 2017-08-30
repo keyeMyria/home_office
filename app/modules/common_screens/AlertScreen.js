a34fd25d2b4029bc71b2f95f59c05ffd593959f9
Gabriel Bessa <gabrielvcbessa@gmail.com>
2017-08-30 01:15:01 -0300

    Added empty alerts warning

--- app/modules/common_screens/AlertScreen.js
+++ app/modules/common_screens/AlertScreen.js
@@ -1,6 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import { observer } from 'mobx-react/native';
+import { View, Image } from 'react-native';
+import { Text } from 'native-base';
 
 // Store
 import avisoStore from './../../stores/AvisoStore';
@@ -8,6 +10,8 @@ import avisoStore from './../../stores/AvisoStore';
 import ScreenShell from './../../components/ScreenShell';
 import CardAlert from './../../components/CardAlert';
 import BubbleMenu from './../../components/BubbleMenu';
+
+const emptyEventsImg = require('../../img/blankCalendar.png');
 
 @observer
 export default class AlertScreen extends Component {
@@ -26,7 +30,34 @@ export default class AlertScreen extends Component {
         return (
           <ScreenShell {...this.screenShellProps}>
             <BubbleMenu />
-            {alerts.map(aviso => <CardAlert key={aviso.id} alert={aviso} />)}
+            {
+                alerts.length ?
+
+                alerts.map(aviso => <CardAlert key={aviso.id} alert={aviso} />)
+
+                :
+
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignSelf: 'stretch',
+                }}
+                >
+                  <Image
+                    source={emptyEventsImg}
+                    style={{
+                        width: 50,
+                        height: 50,
+                        margin: 15,
+                    }}
+                  />
+                  <Text>
+                      Nenhuma aviso cadastrado.
+                    </Text>
+                </View>
+
+                }
           </ScreenShell>
         );
     }
