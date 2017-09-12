864f7935c00a6afaa4419534aca305cc661fd62a
Rayan Sóstenes <rayancocos@gmail.com>
2017-09-12 20:27:42 -0300

    Add EmptyScreen component

---
+++ app/components/EmptyScreen.js
@@ -0,0 +1,50 @@
+// @flow
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default class EmptyScreen extends Component {
+    props: {
+        image: any,
+        title: string,
+        text: string,
+    };
+
+    render() {
+        const { image, title, text } = this.props;
+
+        return (
+          <View style={styles.container}>
+            <Image source={image} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.text}>{text}</Text>
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(239, 239, 239)',
+        alignSelf: 'stretch',
+        padding: 56,
+    },
+    title: {
+        fontSize: 18,
+        color: 'rgba(0,0,0,0.87)',
+        marginBottom: 10,
+    },
+    text: {
+        fontSize: 14,
+        color: 'rgba(0,0,0,0.50)',
+        textAlign: 'center',
+    },
+    image: {
+        width: 128,
+        height: 128,
+        marginBottom: 35,
+        tintColor: 'rgba(0,0,0,0.37)',
+    },
+});
