44effb6dc0752ade98abb982dbebc91939d2f2b1
Rayan Sóstenes <rayancocos@gmail.com>
2017-11-06 04:07:13 -0200

    alterações em "CalendarWeek"

--- app/components/calendar/CalendarWeek.js
+++ app/components/calendar/CalendarWeek.js
@@ -1,32 +1,52 @@
 // @flow
 import React, { PureComponent } from 'react';
-import { View } from 'react-native';
-import { Separator, Text } from 'native-base';
+import { View, Text, StyleSheet } from 'react-native';
+
+type Props = {
+    label: string,
+};
 
 // Components
 export default class CalendarWeek extends PureComponent {
-    props: {
-        label: string,
-    };
+    props: Props;
 
     static defaultProps = {
-        label: '',
+        label: 'INVALID WEEK',
     };
 
     render() {
         const { label } = this.props;
         return (
-          <View>
-            <Separator style={styles.separator}>
-              <Text>{label}</Text>
-            </Separator>
+          <View style={styles.container}>
+            <View style={styles.separator}>
+              <Text style={styles.text}>{label}</Text>
+            </View>
           </View>
         );
     }
 }
 
-const styles = {
-    separator: {
-        paddingLeft: 30,
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'transparent',
+        marginBottom: 10,
+        marginTop: 10,
+        alignItems: 'center',
+        elevation: 4,
     },
-};
+    separator: {
+        backgroundColor: '#C5CAE9',
+        paddingVertical: 0,
+        paddingHorizontal: 12,
+        borderRadius: 8,
+        height: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'rgba(0,0,0,.57)',
+        fontSize: 14,
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+    },
+});
---
+++ app/components/calendar/index.js
@@ -0,0 +1,12 @@
+// @flow
+import CalendarDay from './CalendarDay';
+import CalendarItem from './CalendarItem';
+import CalendarWeek from './CalendarWeek';
+import CalendarModal from './CalendarModal';
+
+export {
+    CalendarDay,
+    CalendarItem,
+    CalendarWeek,
+    CalendarModal,
+};
